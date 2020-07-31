22fc068967e955e0c8430da5e07e10e31e04fe94
Igor Borisov <latentpickuper@gmail.com>
2020-07-31 18:13:14 +0300

    Работаем

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 "use strict";
 
 // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+let vh = window.innerHeight * 0.01;
+
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+
+//Burger menu
+
+$('.menu__btn').click(function () {
+   $('.menu__contacts-mobile').toggleClass('menu__open')
+   $('.menu__btn').toggleClass('menu__btn_open')
+});
+
+$(document.body).click(function () {
+  console.log('dsfdsfsd');
+});
