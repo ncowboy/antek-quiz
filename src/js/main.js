be8ee883c71dcd8d7ba6750c931748cad14ec0f6
Igor Borisov <latentpickuper@gmail.com>
2020-08-05 18:52:37 +0300

    делаем квиз

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,10 @@
 "use strict";
 
-// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-
-let vh = window.innerHeight * 0.01;
-
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
 //Burger menu
 
 $('.menu__btn').click(function () {
-  $('.menu__contacts-mobile').toggleClass('menu__open')
-  $('.menu__btn').toggleClass('menu__btn_open')
+  $('.menu__contacts-mobile').toggleClass('menu__open');
+  $('.menu__btn').toggleClass('menu__btn_open');
 });
 
 
@@ -19,5 +12,36 @@ $('#modal-request').on('hidden.bs.modal', function () {
   setTimeout(function () {
     $('#modal-success').modal('show');
   }, 1000);
+});
+
+$('#modal-quiz').modal('show');
+
+// Quiz
+
+
+$('.quiz__input_range').on('change', function (e) {
+  const btn = $(e.target).parent().next().find('.btn');
+  if (e.target.value !== '0') {
+    btn.attr('disabled', false).removeClass('btn_inactive');
+  } else {
+    btn.attr('disabled', true).addClass('btn_inactive');
+  }
+
+
+  $('.quiz__btn').click(function (e) {
+    const currentBlock = $(e.target).parent().parent();
+    const blockIndex = e.target.dataset.target;
+    const nextBlock =  $('.quiz__form').find(`.quiz__block[data-block=${blockIndex}]`);
+    currentBlock.hide();
+    nextBlock.show();
+  })
+});
+
+$('.quiz__check').on('change', function (e) {
+  const btn = $(e.target).parent().parent().next().find('.btn_inactive');
+  if(btn) {
+    btn.attr('disabled', false).removeClass('btn_inactive');
+  }
+
 });
 
