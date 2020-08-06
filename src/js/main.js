d22c6dd2ca54cb16cc40c9c4038dd1fcdf6a7911
Igor Borisov <latentpickuper@gmail.com>
2020-08-06 19:02:42 +0300

    Готово

--- src/js/main.js
+++ src/js/main.js
@@ -14,10 +14,9 @@ $('#modal-request').on('hidden.bs.modal', function () {
   }, 1000);
 });
 
-$('#modal-quiz').modal('show');
+
 
 // Quiz
-
 
 $('.quiz__input_range').on('change', function (e) {
   const btn = $(e.target).parent().next().find('.btn');
@@ -28,20 +27,42 @@ $('.quiz__input_range').on('change', function (e) {
   }
 
 
-  $('.quiz__btn').click(function (e) {
+$('.quiz__btn').click(function (e) {
     const currentBlock = $(e.target).parent().parent();
     const blockIndex = e.target.dataset.target;
-    const nextBlock =  $('.quiz__form').find(`.quiz__block[data-block=${blockIndex}]`);
+    const nextBlock = $('.quiz__form').find(`.quiz__block[data-block=${blockIndex}]`);
     currentBlock.hide();
     nextBlock.show();
   })
 });
 
 $('.quiz__check').on('change', function (e) {
-  const btn = $(e.target).parent().parent().next().find('.btn_inactive');
-  if(btn) {
-    btn.attr('disabled', false).removeClass('btn_inactive');
+  const btn = $(e.target).parent().parent().next().find('.btn').not('.btn_inactive').not('.quiz__btn_prev');
+  const btn_disabled = $(e.target).parent().parent().next().find('.btn_inactive');
+  const blockInputName = $(e.target).attr('name');
+  let checked = 0;
+
+  $(`.quiz__check[name=${blockInputName}]`).each(function (index, el) {
+    if($(el).get(0).checked) {
+           checked++;
+    }
+  });
+  if (btn_disabled && checked !== 0) {
+    btn_disabled.attr('disabled', false).removeClass('btn_inactive');
   }
 
+  if(btn && checked === 0) {
+    btn.attr('disabled', true).addClass('btn_inactive');
+  }
 });
+
+$('.quiz__form').submit(function (e) {
+  e.preventDefault();
+
+  setTimeout(function () {
+    $('#modal-quiz-success').modal('show');
+  }, 1000);
+});
+
+
 
